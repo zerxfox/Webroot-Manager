8b44b8fae3a60b9b4cc4b643020315d59b7d216a
ZerxFox <36778239+zerxfox@users.noreply.github.com>
2025-04-20 00:48:19 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -5,18 +5,28 @@ do
   sleep 10
 done
 
-su -lp 2000 -c "cmd notification post -S bigtext -t 'Webroot Manager' 'Tag' 'Running on http://localhost:8080' > /dev/null 2>&1"
+LANG=$(settings get system system_locales)
 
-su -c "chmod 777 /data/adb/modules/webroot/files" >> /data/local/tmp/webroot.log 2>&1
-if [ $? -eq 0 ]; then
-    echo "$(date) - Successfully changed permissions for /data/adb/modules/webroot/files" >> /data/local/tmp/webroot.log
+is_russian() {
+  [[ "$LANG" == *"en-RU"* ]] || [[ "$LANG" == *"ru-"* ]]
+}
+
+if su -c "chmod 777 /data/adb/modules/webroot/files" >> /data/local/tmp/webroot.log 2>&1; then
+    result_message="Успешно изменены права для /data/adb/modules/webroot/files"
+    result_message_en="Successfully changed permissions for /data/adb/modules/webroot/files"
 else
-    echo "$(date) - Error changing permissions for /data/adb/modules/webroot/files" >> /data/local/tmp/webroot.log
+    result_message="Ошибка при изменении прав для /data/adb/modules/webroot/files"
+    result_message_en="Error changing permissions for /data/adb/modules/webroot/files"
+fi
+echo "$(date) - $(is_russian && echo "$result_message" || echo "$result_message_en")" >> /data/local/tmp/webroot.log
+
+if su -c "/system/bin/webroot" >> /data/local/tmp/webroot.log 2>&1; then
+    result_message="✅ Webroot Manager успешно запущен [localhost:8080]"
+    result_message_en="✅ Webroot Manager successfully started [localhost:8080]"
+else
+    result_message="❌ Ошибка при запуске Webroot Manager"
+    result_message_en="❌ Error starting Webroot"
 fi
 
-su -c "/system/bin/webroot" >> /data/local/tmp/webroot.log 2>&1
-if [ $? -eq 0 ]; then
-    echo "$(date) - Webroot successfully started" >> /data/local/tmp/webroot.log
-else
-    echo "$(date) - Error starting Webroot" >> /data/local/tmp/webroot.log
-fi
+sed -i '6,7d' /data/adb/modules/webroot/module.prop
+echo "description=[$(date)] $(is_russian && echo "$result_message" || echo "$result_message_en")" >> /data/adb/modules/webroot/module.prop
